2f96bb557008057f6c8f1c61ef34b40b2a93b849
Chao sijian <34854946+ChaosJian@users.noreply.github.com>
2021-04-06 10:09:05 +0800

    Update leetcode80.py

--- leetcode80.py
+++ leetcode80.py
@@ -1,3 +1,9 @@
+## 题目链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+## 题目描述：
+## 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 最多出现两次 ，返回删除后数组的新长度。
+## 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
@@ -20,3 +26,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+## 输出：nums= [1, 2, 1, 2, 2, 3]
+##      nums= [1, 2, 3, 2, 2, 3]
+## 疑问：上面的完整程序在pycharm中得到如上输出，但是在leetcode中得到的结果是[1,2,3],不知道nums中后面的数[2,2,3]怎么删去的呢？
